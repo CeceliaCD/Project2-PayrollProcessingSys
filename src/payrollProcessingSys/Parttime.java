433b2d71b2240537e1a086fdee941d9028c2419a
ansanida <33883725+ansanida@users.noreply.github.com>
2021-02-17 21:34:30 -0500

    Update Parttime.java

--- src/payrollProcessingSys/Parttime.java
+++ src/payrollProcessingSys/Parttime.java
@@ -8,12 +8,19 @@ public class Parttime extends Employee {
 	private int parttimeTotalHrs = 80; //The 2 week pay period
 	public DecimalFormat hourlyRate = new DecimalFormat("##.##"); //Whatever parttimer is paid per hour
 	public static final double OVERTIME = 1.5; //Represents time and a half regular hourly for more than 80 hours
+	private int hoursWorked; // hours that a parttimer worked; does this need to be an int?
 	
 	//Right now I don't think these subclass constructors are necessary since there is only employee objects
 	public Parttime() {
 		// TODO Auto-generated constructor stub
 	}
+	public void setHourlyRate(DecimalFormat rate) {
+		this.hourlyRate = rate;
+	}
 	
+	public void setHoursWorked(int hours) {
+		this.hoursWorked = hours;
+	}
 	@Override
 	public void calculatePayment() { 
 		
