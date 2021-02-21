1c669d21cfa41dac8b9209aa6dbbb9a8b07c6644
ansanida <33883725+ansanida@users.noreply.github.com>
2021-02-20 22:36:54 -0500

    Update PayrollProcessing.java

--- src/payrollProcessingSys/PayrollProcessing.java
+++ src/payrollProcessingSys/PayrollProcessing.java
@@ -9,17 +9,75 @@ public class PayrollProcessing {
 		Scanner userInput = new Scanner(System.in);
 		Company company = new Company();
 		
-		String commandLine = userInput.nextLine();
-		userInput.useDelimiter(" ");
+		//String commandLine = userInput.nextLine();
+		//userInput.useDelimiter(" ");
 		
-		while(userInput.hasNext(commandLine)) {
-			if(commandLine.equals("")) {
-				System.out.println("Command" + " '" + commandLine + "' not supported!");
+		//while(userInput.hasNext(commandLine)) {
+			//if(commandLine.equals("")) {
+				//System.out.println("Command" + " '" + commandLine + "' not supported!");
+			//}
+			
+		//}
+		
+		while (true) {
+
+			String choice = userInput.nextLine();
+			String[] command = choice.split(" ");
+			String letter = command[0];
+
+			if (letter.equals("AP")) { // add a part time employee
+				//System.out.println("hello");
+				String name = command[1];
+				String dept = command[2];
+				Date dateHired = new Date(command[3]);
+				
+				if (dateHired.isValid() == true) {
+					Profile emp = new Profile();
+					emp.setName(name);
+					emp.setDepartment(dept);
+					emp.setDateHired(dateHired);
+
+				}
+			}
+
+			if (letter.equals("AF")) { // add a full time employee
+
 			}
 			
-		}
-		
-		
-	}
+			if (letter.equals("AM")) { // add a full time manager
 
+			}
+			
+			if (letter.equals("R")) { // remove an employee
+
+			}
+			
+			if (letter.equals("C")) { //calculate payments for all employees
+
+			}
+			
+			if (letter.equals("S")) { // set the working hours
+
+			}
+			
+			if (letter.equals("PA")) { // print earning statements for all employees
+
+			}
+			
+			if (letter.equals("PH")) { // print in order of date hired
+
+			}
+			
+			if (letter.equals("PD")) { // print grouped by department
+
+			}
+			
+			if (letter.equals("Q")) {
+				System.out.println("Payroll Processing Completed.");
+				System.exit(0);
+			} 
+
+		} 
+
+	}
 }
