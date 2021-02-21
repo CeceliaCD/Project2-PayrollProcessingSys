a0f88c3eb2c3658dfebaa5e8f14b8a32529b7c36
CeceliaCD <cecelia.chollettedickson@rutgers.edu>
2021-02-21 18:11:14 -0500

    Added JUnit test files: CompanyTest and ManagementTest.

---
+++ src/payrollProcessingSys/CompanyTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package payrollProcessingSys;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author ceceliachollette-dickson
+ *
+ */
+public class CompanyTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link payrollProcessingSys.Company#add(payrollProcessingSys.Employee)}.
+	 */
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link payrollProcessingSys.Company#remove(payrollProcessingSys.Employee)}.
+	 */
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link payrollProcessingSys.Company#setHours(payrollProcessingSys.Employee)}.
+	 */
+	@Test
+	public void testSetHours() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/payrollProcessingSys/ManagementTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package payrollProcessingSys;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author ceceliachollette-dickson
+ *
+ */
+public class ManagementTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link payrollProcessingSys.Management#calculatePayment()}.
+	 */
+	@Test
+	public void testCalculatePayment() {
+		fail("Not yet implemented");
+	}
+
+}
